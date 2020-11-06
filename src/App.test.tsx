2b4d710cfe1f12845d8e28bbd534bafc7ea0419f
Adam Morris <adam@kernelpanicstudios.com>
2020-11-06 16:41:26 -0600

    Fixing unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { waitFor } from '@testing-library/react';
-import App from './App';
-import defaultParticipant from './mocks/default-participant.json';
-import { Render } from './test-utils';
-import { fetchParticipantById } from './services/ExtraLife';
+import React from "react";
+import { waitFor } from "@testing-library/react";
+import App from "./App";
+import defaultParticipant from "./mocks/default-participant.json";
+import { Render } from "./test-utils";
+import { fetchParticipantById } from "./services/ExtraLife";
 
 const defaultTestState = {
   state: {
     participant: {
-      id: 111333
-    }
-  }
+      id: 111333,
+    },
+    team: {},
+  },
 };
 
 // These are more or less smoke tests for CI. More comprehensive tests will be coming.
@@ -19,24 +20,32 @@ describe("renders app", () => {
     fetchMock.resetMocks();
   });
 
-  test('without crashing', async () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("without crashing", async () => {
+    jest
+      .spyOn(URLSearchParams.prototype, "get")
+      .mockImplementation((name) => name);
+
     fetchMock.mockResponse(JSON.stringify(defaultParticipant));
 
     const { queryByTestId } = Render(<App />, defaultTestState);
 
     await waitFor(() => fetchMock.mock.calls.length > 0, { timeout: 3000 });
 
-    expect(queryByTestId('logo')).toBeInTheDocument()
-    expect(queryByTestId('progress')).toBeInTheDocument()
+    expect(queryByTestId("logo")).toBeInTheDocument();
+    expect(queryByTestId("progress")).toBeInTheDocument();
   });
 
-  test('without crashing and donor drive unavailable', () => {
-    fetchMock.mockRejectOnce(new Error("failed!"))
+  test("without crashing and donor drive unavailable", () => {
+    fetchMock.mockRejectOnce(new Error("failed!"));
 
     const { queryByTestId } = Render(<App />, { state: {} });
 
-    expect(queryByTestId('logo')).toBeInTheDocument()
-    expect(queryByTestId('progress')).not.toBeInTheDocument();
+    expect(queryByTestId("logo")).toBeInTheDocument();
+    expect(queryByTestId("progress")).not.toBeInTheDocument();
   });
 
   // TODO: Smoke tests for orientation.
