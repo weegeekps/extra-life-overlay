d32b5aec90b5c45cd18c1e2b8a3537ee03520d93
Adam Morris <adam@kernelpanicstudios.com>
2021-10-29 15:20:43 -0500

    Added support for showing Team Name and the Fundraising Goal (#18)
    * Added ability to display team name and fundraising goal.
    
    * Cleaned up some imports.

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,18 +1,30 @@
-import React from "react";
 import { connect } from "react-redux";
 import { useSpring, animated } from "react-spring";
 import "./Progress.css";
 import { IAppState } from "../models/IAppState";
 
+export interface IProgressOptions {
+  showTeamName: boolean;
+  showGoal: boolean;
+}
+
 export interface IProgressOwnProps {
   className?: string;
+  options?: IProgressOptions;
 }
 
 export interface IProgressProps {
   className?: string;
   sumDonations: number;
   fundraisingGoal: number;
+  teamName?: string;
+  showGoal: boolean;
 }
+
+const createDefaultOptions = (): IProgressOptions => ({
+  showTeamName: false,
+  showGoal: false,
+});
 
 const clamp = (value: number, min: number, max: number) => {
   if (value > min && value < max) return value;
@@ -24,7 +36,13 @@ const calculateCompletedWidth = (current: number, goal: number) =>
   clamp((current / goal) * 100, 0, 100);
 
 const Progress: React.FC<IProgressProps> = (props) => {
-  const { className: classes, sumDonations, fundraisingGoal } = props;
+  const {
+    className: classes,
+    sumDonations,
+    fundraisingGoal,
+    teamName,
+    showGoal,
+  } = props;
 
   const hasValues = fundraisingGoal !== 0 && sumDonations >= 0;
 
@@ -37,13 +55,28 @@ const Progress: React.FC<IProgressProps> = (props) => {
   if (!hasValues) return null;
 
   return (
-    <div className="progress-container" data-testid="progress">
-      <div className={"progress-bar " + classes}>
-        <animated.div className="completed" style={springWidth}></animated.div>
+    <div className={"progress-region"}>
+      {teamName && (
+        <div className={"progress-team-name " + classes}>
+          <p>{teamName}</p>
+        </div>
+      )}
+      <div className="progress-container" data-testid="progress">
+        <div className={"progress-bar " + classes}>
+          <animated.div
+            className="completed"
+            style={springWidth}
+          ></animated.div>
+        </div>
+        <div className={"progress-text " + classes}>
+          <p>${sumDonations.toFixed(2)}</p>
+        </div>
       </div>
-      <div className={"progress-text " + classes}>
-        <p>${sumDonations.toFixed(2)}</p>
-      </div>
+      {showGoal && (
+        <div className={"progress-goal " + classes}>
+          <p>${fundraisingGoal.toFixed(2)}</p>
+        </div>
+      )}
     </div>
   );
 };
@@ -53,7 +86,7 @@ const mapStateToProps = (state: IAppState, ownProps: IProgressOwnProps) => {
     participant: { value: participant },
     team: { value: team },
   } = state;
-  const { className } = ownProps;
+  const { className, options } = ownProps;
 
   const sumDonations = participant
     ? participant.sumDonations
@@ -66,10 +99,14 @@ const mapStateToProps = (state: IAppState, ownProps: IProgressOwnProps) => {
     ? team.fundraisingGoal
     : 0;
 
+  const safeOptions = options || createDefaultOptions();
+
   return {
     className,
     sumDonations,
     fundraisingGoal,
+    teamName: safeOptions.showTeamName ? team?.name : undefined,
+    showGoal: safeOptions.showGoal,
   };
 };
 
