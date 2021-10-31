1addb668174c72055a0f9f114ad3e3027e132f48
Adam Morris <adam@kernelpanicstudios.com>
2021-10-31 16:14:49 -0500

    Updated default logo and added support for choosing from one of three designs. (#19)

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,26 @@
 import xss from "xss";
+import { ILogoChoice } from "./models/ILogoChoice";
+
+type EnumTypes = ILogoChoice;
 
 export const getQueryStringValue = (queryString: string) => {
-  const sanitized = xss(queryString)
+  const sanitized = xss(queryString);
 
   return (
     new URLSearchParams(window.location.search).get(sanitized) || undefined
   );
 };
+
+export function getQueryStringEnumValue<T extends EnumTypes>(
+  queryString: string
+): T | undefined {
+  const safeQueryString = getQueryStringValue(queryString);
+  if (safeQueryString) {
+    return safeQueryString as T;
+  }
+
+  return undefined;
+}
 
 export const checkQueryStringBoolean = (queryString: string) => {
   return getQueryStringValue(queryString) === "true";
