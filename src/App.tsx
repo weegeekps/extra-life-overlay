9383ca55874347d505c7777c17b7254a2c88f250
Adam Morris <adam@kernelpanicstudios.com>
2021-10-31 16:06:36 -0500

    Updated default logo and added support for choosing from one of three designs.

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import {
   getQueryStringValue,
   checkQueryStringBoolean,
   prepareClassString,
+  getQueryStringEnumValue,
 } from "./utils";
 import { Orientation } from "./models/Orientation";
+import { ILogoChoice } from "./models/ILogoChoice";
 
 const App: React.FC = (props) => {
   const orientation = getQueryStringValue("orientation");
   const debuggingMode = checkQueryStringBoolean("debugging");
   const showTeamName = checkQueryStringBoolean("showTeamName");
   const showGoal = checkQueryStringBoolean("showGoal");
+  const logoChoice = getQueryStringEnumValue<ILogoChoice>("logo");
   const topLevelClasses = prepareClassString(
     "app",
     orientation || Orientation.Left,
@@ -21,7 +24,7 @@ const App: React.FC = (props) => {
 
   return (
     <div className={topLevelClasses} data-testid="app">
-      <Logo />
+      <Logo choice={logoChoice} />
       <Progress
         className={orientation || Orientation.Left}
         options={{
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,32 @@
-import React from "react";
 import "./Logo.css";
-import logo from "../controller_white.svg";
+import controllerLogo2020 from "../logos/2020_controller_white.svg";
+import wingsLogo2021 from "../logos/2021_wings_white.svg";
+import diceLogo2021 from "../logos/2021_dice_white.svg";
+import { ILogoChoice } from "../models/ILogoChoice";
 
-const Logo: React.FC = (props) => {
+export interface ILogoProps {
+  choice?: ILogoChoice;
+}
+
+const chooseLogo = (choice?: ILogoChoice): [string, string] => {
+  switch (choice) {
+    case ILogoChoice.Controller2020:
+      return [controllerLogo2020, "logo-2020"];
+    case ILogoChoice.Dice2021:
+      return [diceLogo2021, "logo-dice-2021"];
+    case ILogoChoice.Wings2021:
+    default:
+      return [wingsLogo2021, "logo-wings-2021"];
+  }
+};
+
+const Logo: React.FC<ILogoProps> = (props) => {
+  const { choice } = props;
+
+  const [logo, className] = chooseLogo(choice);
+
   return (
-    <div className="logo" data-testid="logo">
+    <div className={"logo " + className} data-testid="logo">
       <img src={logo} alt="Extra Life" />
     </div>
   );
